68de2351213395e50a4546df0ba09190c2ceca7d
REKKAB Amel <arekkab@jsi-groupe.com>
2017-06-16 17:14:43 +0200

    new projet

---
+++ src/main/java/org/gestion/banque/dao/IBannqueDao.java
@@ -0,0 +1,35 @@
+package org.gestion.banque.dao;
+
+
+
+import java.util.List;
+
+import org.gestion.banque.entities.Client;
+import org.gestion.banque.entities.Compte;
+import org.gestion.banque.entities.Employe;
+import org.gestion.banque.entities.Groupe;
+import org.gestion.banque.entities.Operation;
+
+import com.mysql.jdbc.log.Log;
+
+public interface IBannqueDao {
+	public Client addClient(Client c);
+	public Employe addEmploye(Employe e, long codeSup);
+	public Groupe addGroupe(Groupe g);
+	public void addEmployeToGroupe(Log codeEmp, Long codeGr);
+	public Compte addCompte(Compte cp,Long codeCli, Long codeEmp);
+	public Operation addOperation(Operation op,String codeCpte, Long codeEmp);
+	//public void versement(String codeCopte, double mt, Long codeEmp );
+	//public void retrait(String codeCopte, double mt, Long codeEmp);
+	//public void virement(String codeCpte1, String Cpte2,  double mt, Long codeEmp);
+	public Compte consulterCompte(String codeCpte);
+	public List<Operation> consulterOperations(String codeCpte);
+	public Client consulterClient(Long codeCli);
+	public List<Client> consulterClients(String mc);
+	public List<Compte> getComptesByClient(Long codeCli);
+	public List<Compte> getComptesByEmploye(Long codeEmp);
+	public List<Employe> getEmployes();
+	public List<Groupe> getGroupes();
+	public List<Employe> getEmployesByGroupe(Long codeGr);
+	
+}
