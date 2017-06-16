37c1670a237917fbef438ee490f506bd71be846e
REKKAB Amel <arekkab@jsi-groupe.com>
2017-06-16 16:40:05 +0200

    new projet

---
+++ src/main/java/org/gestion/banque/entities/Retrait.java
@@ -0,0 +1,22 @@
+package org.gestion.banque.entities;
+
+import java.sql.Date;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue(value="R")
+public class Retrait extends Operation {
+
+	public Retrait() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Retrait(Date dateOperation, double montant) {
+		super(dateOperation, montant);
+		// TODO Auto-generated constructor stub
+	}
+
+}
