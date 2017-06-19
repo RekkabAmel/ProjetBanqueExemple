9a043c23a883af9c6eadf9c5af1fc45d6c35f66b
RekkabAmel <rekkabamel@yahoo.fr>
2017-06-19 12:01:59 +0200

    creation de la couche metier

---
+++ src/main/java/org/gestion/banque/metier/BanqueMetierImpl.java
@@ -0,0 +1,133 @@
+package org.gestion.banque.metier;
+
+
+import java.util.Date;
+import java.util.List;
+
+import org.gestion.banque.dao.IBanqueDao;
+import org.gestion.banque.entities.Client;
+import org.gestion.banque.entities.Compte;
+import org.gestion.banque.entities.Employe;
+import org.gestion.banque.entities.Groupe;
+import org.gestion.banque.entities.Operation;
+import org.gestion.banque.entities.Retrait;
+import org.gestion.banque.entities.Versement;
+
+import com.mysql.jdbc.log.Log;
+
+@Transactional
+public class BanqueMetierImpl implements IBanqueMetier{
+	
+	private IBanqueDao dao;
+	
+	public void setDao(IBanqueDao dao) {
+		this.dao = dao;
+	}
+	
+	@Override
+	public Client addClient(Client c) {
+		// TODO Auto-generated method stub
+		return dao.addClient(c);
+	}
+
+	@Override
+	public Employe addEmploye(Employe e, Long codeSup) {
+		// TODO Auto-generated method stub
+		return dao.addEmploye(e, codeSup);
+	}
+
+	@Override
+	public Groupe addGroupe(Groupe g) {
+		// TODO Auto-generated method stub
+		return dao.addGroupe(g);
+	}
+
+	@Override
+	public void addEmployeToGroupe(Log codeEmp, Long codeGr) {
+		dao.addEmployeToGroupe(codeEmp, codeGr);	
+	}
+
+	@Override
+	public Compte addCompte(Compte cp, Long codeCli, Long codeEmp) {
+		// TODO Auto-generated method stub
+		return dao.addCompte(cp, codeCli, codeEmp);
+	}
+
+	@Override
+	public void verser(double mt, String cpte, Long codeEmp) {
+		dao.addOperation(new Versement((java.sql.Date) new Date(),mt), cpte, codeEmp);
+		Compte cp=dao.consulterCompte(cpte);
+		cp.setSolde(cp.getSolde()+mt);
+		
+	}
+
+	@Override
+	public void retirer(double mt, String cpte, Long codeEmp) {
+		dao.addOperation(new Retrait((java.sql.Date) new Date(),mt), cpte, codeEmp);
+		Compte cp=dao.consulterCompte(cpte);
+		cp.setSolde(cp.getSolde()-mt);
+		
+	}
+
+	@Override
+	public void virement(double mt, String cpte1, String cpte2, Long codeEmp) {
+		retirer(mt, cpte1,codeEmp);
+		verser(mt, cpte2, codeEmp);
+		
+	}
+
+	@Override
+	public Compte consulterCompte(String codeCpte) {
+		
+		return dao.consulterCompte(codeCpte);
+	}
+
+	@Override
+	public List<Operation> consulterOperations(String codeCpte) {
+		
+		return dao.consulterOperations(codeCpte);
+	}
+
+	@Override
+	public Client consulterClient(Long codeCli) {
+		
+		return dao.consulterClient(codeCli);
+	}
+
+	@Override
+	public List<Client> consulterClients(String mc) {
+		
+		return dao.consulterClients(mc);
+	}
+
+	@Override
+	public List<Compte> getComptesByClient(Long codeCli) {
+		
+		return dao.getComptesByClient(codeCli);
+	}
+
+	@Override
+	public List<Compte> getComptesByEmploye(Long codeEmp) {
+		
+		return dao.getComptesByEmploye(codeEmp);
+	}
+
+	@Override
+	public List<Employe> getEmployes() {
+		
+		return dao.getEmployes();
+	}
+
+	@Override
+	public List<Groupe> getGroupes() {
+		
+		return dao.getGroupes();
+	}
+
+	@Override
+	public List<Employe> getEmployesByGroupe(Long codeGr) {
+		
+		return dao.getEmployesByGroupe(codeGr);
+	}
+
+}
---
+++ src/main/java/org/gestion/banque/metier/IBanqueMetier.java
@@ -0,0 +1,33 @@
+package org.gestion.banque.metier;
+
+import java.util.List;
+
+import org.gestion.banque.entities.Client;
+import org.gestion.banque.entities.Compte;
+import org.gestion.banque.entities.Employe;
+import org.gestion.banque.entities.Groupe;
+import org.gestion.banque.entities.Operation;
+
+import com.mysql.jdbc.log.Log;
+
+public interface IBanqueMetier {
+
+	public Client addClient(Client c);
+	public Employe addEmploye(Employe e, Long codeSup);
+	public Groupe addGroupe(Groupe g);
+	public void addEmployeToGroupe(Log codeEmp, Long codeGr);
+	public Compte addCompte(Compte cp,Long codeCli, Long codeEmp);
+	public void verser(double mt, String cpte, Long codeEmp );
+	public void retirer(double mt, String cpte, Long codeEmp);
+	public void virement(double mt, String cpte1, String cpte2, Long codeEmp);
+	public Compte consulterCompte(String codeCpte);
+	public List<Operation> consulterOperations(String codeCpte);
+	public Client consulterClient(Long codeCli);
+	public List<Client> consulterClients(String mc);
+	public List<Compte> getComptesByClient(Long codeCli);
+	public List<Compte> getComptesByEmploye(Long codeEmp);
+	public List<Employe> getEmployes();
+	public List<Groupe> getGroupes();
+	public List<Employe> getEmployesByGroupe(Long codeGr);
+	
+}
