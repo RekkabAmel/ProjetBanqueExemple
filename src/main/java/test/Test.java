5bdfe1a08edcc9a1cc13a6260ff2482d068eb109
RekkabAmel <rekkabamel@yahoo.fr>
2017-06-20 16:19:18 +0200

    added class de test

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.gestion.banque.entities.Client;
+import org.gestion.banque.metier.IBanqueMetier;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main (String[] args){
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext(new String[]{"applicationCotext.xml"});
+		IBanqueMetier metier = (IBanqueMetier)context.getBean("metier");
+		metier.addClient(new Client("C1","AD1"));
+		metier.addClient(new Client("C2", "AD2"));
+		
+	}
+
+}
