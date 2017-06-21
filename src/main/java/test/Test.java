3918a476198e9b917f0ef392d5607ec326e3e5f4
RekkabAmel <rekkabamel@yahoo.fr>
2017-06-21 12:44:43 +0200

    modifier Test.java

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 	public static void main (String[] args){
 		ClassPathXmlApplicationContext context =
-				new ClassPathXmlApplicationContext(new String[]{"applicationCotext.xml"});
+				new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml"});
 		IBanqueMetier metier = (IBanqueMetier)context.getBean("metier");
 		metier.addClient(new Client("C1","AD1"));
 		metier.addClient(new Client("C2", "AD2"));
